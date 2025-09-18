522dff1d4d5cd79a16879e9c214332f403b8d715
Damian Heart <vbsl.chanakya6@gmail.com>
2025-09-18 15:29:44 +0530

    removed unwanted files.

--- src/assets/images/index.ts
+++
@@ -1,7 +0,0 @@
-// Extension Event Images
-// These will be the actual image files provided by the user
-export const extensionEventImage1 = '/src/assets/images/extension-event-1.png';
-export const extensionEventImage2 = '/src/assets/images/extension-event-2.png';
-
-// Placeholder for KL University logo (to be added)
-export const klUniversityLogo = '/src/assets/images/kl-university-logo.png';
